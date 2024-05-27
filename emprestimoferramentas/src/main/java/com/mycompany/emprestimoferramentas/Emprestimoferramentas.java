7946c7d78856e73420e05c57fe51756c225b5e46
Joaoaugustohm <joaom.homemjah@gmail.com>
2024-05-27 19:27:16 -0300

    criar package

---
+++ emprestimoferramentas/src/main/java/com/mycompany/emprestimoferramentas/Emprestimoferramentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.emprestimoferramentas;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Emprestimoferramentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
